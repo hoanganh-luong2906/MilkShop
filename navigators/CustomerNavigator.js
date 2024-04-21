6609478a187577c4d64883e81f3a45d84987fc56
kietpt2003 <tuankiet29012003@gmail.com>
2024-04-21 08:30:31 +0700

    fix conflict

--- navigators/CustomerNavigator.js
+++ navigators/CustomerNavigator.js
@@ -1,5 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LandingScreen from '../screens/admin/LandingScreen';
+import LandingScreen from '../screens/LandingScreen';
 
 const Tab = createBottomTabNavigator();
 
