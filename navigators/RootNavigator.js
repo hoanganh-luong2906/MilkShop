b79ffc9840bf1e95612b6945d61ae59306b5b381
kietpt2003 <tuankiet29012003@gmail.com>
2024-04-23 14:34:37 +0700

    finish register account

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -11,11 +11,9 @@ import MainNavigator from './MainNavigator';
 import StaffNavigator from './StaffNavigator';
 import ProfileScreen from '../screens/ProfileScreen';
 import AddProduct from '../components/admin/AddProduct';
-import { Text, View } from 'react-native';
 import LoadingScreen from '../screens/Loading';
 import UpdateProduct from '../components/admin/UpdateProduct';
 import AddVoucher from '../components/staff/AddVoucher';
-import UpdateVoucher from '../components/staff/UpdateVoucher';
 import CartScreen from '../screens/CartScreen';
 
 const Stack = createNativeStackNavigator();
@@ -49,7 +47,19 @@ const RootNavigator = () => {
 								name='register'
 								component={RegisterScreen}
 								options={{
-									headerShown: false,
+									headerTitle: 'ĐĂNG KÝ',
+									headerShown: true,
+									headerTitleAlign: 'center',
+									headerTintColor: 'black',
+									headerStyle: {
+										backgroundColor: '#FFBE98',
+									},
+									headerTitleStyle: {
+										fontSize: 25,
+										fontWeight: 'bold',
+									},
+									statusBarColor: 'black',
+									headerBackVisible: false
 								}}
 							/>
 							<Stack.Screen
@@ -130,21 +140,6 @@ const RootNavigator = () => {
 										component={AddVoucher}
 										options={{
 											headerTitle: 'Thêm mã giảm giá',
-											headerShown: true,
-											headerTitleAlign: 'center',
-											headerTintColor: 'black',
-											headerStyle: {
-												backgroundColor: '#FFBE98',
-											},
-											headerTitleStyle: { fontSize: 25 },
-											statusBarColor: 'black',
-										}}
-									/>
-									<Stack.Screen
-										name='staff-update-voucher'
-										component={UpdateVoucher}
-										options={{
-											headerTitle: 'Cập nhật mã giảm giá',
 											headerShown: true,
 											headerTitleAlign: 'center',
 											headerTintColor: 'black',
